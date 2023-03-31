3fa8bbd6b5706bef29fa9b7f7a922fb562b5a0f4
Jiachen_Liu <90797729+jiachenliu1128@users.noreply.github.com>
2023-03-30 23:24:10 -0400

    mian

---
+++ main.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main{int argc,char* argv[]} {
+    // command line options
+    bool testMode = false;
+    if (int i = 1; i < argc; i++) {
+
+    }
+}
