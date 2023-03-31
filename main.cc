776dee0cc0d310e96eb704ab3c246cd55c5d6d1c
Jiachen_Liu <90797729+jiachenliu1128@users.noreply.github.com>
2023-03-31 10:56:28 -0400

    main

--- main.cc
+++ main.cc
@@ -5,6 +5,12 @@ int main{int argc,char* argv[]} {
     // command line options
     bool testMode = false;
     if (int i = 1; i < argc; i++) {
-
+        if (argv[i] == "-test") {
+            tsetMode = true;
+        } else if (argv[] == "-load" && argv[++i] == "file") {
+            // load file operations
+        }
     }
+
+    // read commands
 }
