d18064ce684e0b2a02103ecb0cb7666ef6e60128
Jiachen_Liu <90797729+jiachenliu1128@users.noreply.github.com>
2023-03-31 20:41:35 -0400

    main

--- main.cc
+++ main.cc
@@ -17,13 +17,36 @@ int main{int argc,char* argv[]} {
     while (cin >> cmd) {
         if (cmd == "roll") {
             // roll dice and move
+
         } else if (cmd == "next") {
             // move to next 
+            
         } else if (cmd == "trade") {
             string name = " ";
             string give = " ";
             string receive = " ";
             cin >> name >> give >> receive;
+            // check it's a valid player name
+
+            if ((give[0] >= '0' && give[0] <= '9' && 
+            !(receive[0] >= '0' && receive[0] <= '9')) {
+                // check receive is valid building name
+                // trade from money to building
+
+            } else if (!(give[0] >= '0' && give[0] <= '9') && 
+            receive[0] >= '0' && receive[0] <= '9')) {
+                // check give is valid building name
+                //  trade from building to money
+
+            } else if (!(give[0] >= '0' && give[0] <= '9') && 
+            !(receive[0] >= '0' && receive[0] <= '9')) {
+                // check both are valid building name
+                // trade from building to building
+
+            } else {
+                // give money and receive money
+                cerr << "Cannot trade money to moeny" << endl;
+            }
         }
     }
 }
